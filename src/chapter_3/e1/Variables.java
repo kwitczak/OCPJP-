7c75df90e7a1e3bfbd435b6c61adcd9bbb7d6455
Krzysztof Witczak <kwitczak@soldevelo.com>
2015-10-04 19:40:37 +0200

    Chapter 3, exercise 1

---
+++ src/chapter_3/e1/Variables.java
@@ -0,0 +1,16 @@
+class Variables{
+int number = 1_000_000;
+int binary = 0b00101;
+int wrongbinary = 0b0111;
+int octal = 03412;
+int hextor = 0xaaa;
+double a = 123.123;
+float b = 123.131f;
+short c = (short)1236;
+short d = 1;
+byte e = 1 + 1;
+byte f = 29;
+byte a1 = 3;
+byte a2 = 1;
+byte a3 = (byte)(a1 + a2);
+}
