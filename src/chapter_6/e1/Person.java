67c5b80fd1232239eaffa1d16e9a0c61c94cb18c
Krzysztof Witczak <kwitczak@soldevelo.com>
2015-10-25 15:44:28 +0100

    Chapter 6, exercise 1

---
+++ src/chapter_6/e1/Person.java
@@ -0,0 +1,15 @@
+class Person{
+	public static void main(String args[]){
+		byte a = 1;
+		switch(a){
+		case 0:
+		case 1:
+			{}
+			
+		System.out.println("Do I ever print?");
+		case 150:
+		default:
+			System.out.println("Default");
+	}
+}
+}
