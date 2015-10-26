19e2dbe1df9217675e9b3501516db27bc67123ff
Krzysztof Witczak <kwitczak@soldevelo.com>
2015-10-26 21:34:14 +0100

    Chapter 6, exercise 12

---
+++ src/chapter_6/e12/Person.java
@@ -0,0 +1,15 @@
+class Person{
+	public static void main(String args[]){
+		
+		int a = 5 + 4;
+		final int b = 4 + 1;
+		final int c;
+		c = 23;
+		switch(54){
+		
+		case a:
+		case b:
+		case c:
+		}
+	}
+}
